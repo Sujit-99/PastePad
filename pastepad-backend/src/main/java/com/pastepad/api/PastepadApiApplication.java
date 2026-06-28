69486ddb46c6732859b8d363ea7933cee189cc6f
Sujit <sujitkumarmahanta797@gmail.com>
2026-06-28 09:04:28 +0530

    setup done

---
+++ pastepad-backend/src/main/java/com/pastepad/api/PastepadApiApplication.java
@@ -0,0 +1,13 @@
+package com.pastepad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastepadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastepadApiApplication.class, args);
+	}
+
+}
---
+++ pastepad-backend/src/test/java/com/pastepad/api/PastepadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pastepad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastepadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
